f6f4783e6682a2386257996cd4e973bc7cb8ca5e
jokong27 <joshuaanthony27@gmail.com>
2023-01-27 12:34:36 +0800

    search update

--- search.js
+++ search.js
@@ -7,7 +7,7 @@ const options = {
 };
 
 
-let excercise2 = [];
+let exercise2 = [];
 let curArr = 0;
 let minArr = 0;
 let maxArr = 11;
@@ -16,6 +16,7 @@ let page = 1; // initialize page to 1
 let exercisePerPage = 12; // number of games to display per page
 let pageCount = 0;
 let curPage = 1;
+
 
 let searchCounter = 0;
 
@@ -31,27 +32,25 @@ const searchBtn2 = () =>{
 const searchBtn = () => {
 
     async function getNames(names) {
-        const response = await fetch(`https://exerciseapi3.p.rapidapi.com/search/?name=${names}', options)`, options);
+        const response = await fetch(`https://exerciseapi3.p.rapidapi.com/search/?name=${names}Barbell%20Bench%20Press', options)`, options);
         const data = await response.json();
+
         return data;
     }
 
     async function displayNames() {
-        let names = await getNames(names);
+        let names = await getNames();
         pageCount = names.length % 12 > 1 ? (names.length - names.length % 12) / 12 + 1 : false;
 
         let inputValueOrg = searchInputValue;
         let inputValue = searchInputValue.toLowerCase().split("");
 
-        
-
-        //x assigned for array counter of games (games' value inside games)
      
             
-            searchCounter == 0 ? document.getElementById("errorCatcher").innerHTML = `<h4 class="orangeText">Sorry! <span class="text-light">Didnt find anything for</span>  "${inputValue.join("")}"</h4>` : ``
+            searchCounter == 0 ? document.getElementById("errorCatcher").innerHTML = `<h4 class="orangeText">Sorry! <span>Didnt find anything for</span>  "${inputValue.join("")}"</h4>` : ``
             inputValue.length < 2 ? document.getElementById("errorCatcher").innerHTML = `<h4 class="orangeText">**Please enter at least 2 characters!</h4>`: console.log("See");
-            for (let x = 0; x <= games.length; x++) {
-                let gameTitle = games[x].title.toLowerCase().split("")
+            for (let x = 0; x <= names.length; x++) {
+                let gameTitle = names[x].title.toLowerCase().split("")
                 for(let inputValueAry = 0; inputValueAry < inputValue.length ; inputValueAry++){
                         for(let gameTitleAry = 0; gameTitleAry < gameTitle.length ; gameTitleAry++){
                             if(inputValue[0] == gameTitle[gameTitleAry]){
@@ -67,10 +66,10 @@ const searchBtn = () => {
                                             console.log(searchCounter);
                                 
                                             const icon = () => {
-                                                if (games[x].platform == "PC (Windows)") {
+                                                if (names[x].platform == "PC (Windows)") {
                                                   return `<i class="fa-brands fa-windows"></i>
                                                   `;
-                                                } else if (games[x].platform == "Web Browser") {
+                                                } else if (names[x].platform == "Web Browser") {
                                                   return `<i class="fa-regular fa-window-maximize"></i>`;
                                                 } else {
                                                   return `<i class="fa-brands fa-windows"></i> <i class="fa-regular fa-window-maximize"></i>`;
@@ -84,18 +83,18 @@ const searchBtn = () => {
                                         
                                         <small  class="col-lg-3 col-md-6 card-group p-2 rounded-0 text-start">
                                             <div class="card text-bg-dark shadow rounded-0 imgZoom" style="overflow: hidden;">
-                                                <img src=${games[x].thumbnail} class="card-img-top rounded-0" alt="Loading...">
+                                                <img src=${names[x].thumbnail} class="card-img-top rounded-0" alt="Loading...">
                                                 <div class="card-img-overlay d-flex align-items-start rounded-0 cardOvrLay" style="
                                                 height: 197px;">
-                                                <small class="card-text mb-4 orangeBg ps-lg-2 pe-lg-2 p-lg-1 m-0">${games[x].genre}</small>
+                                                <small class="card-text mb-4 orangeBg ps-lg-2 pe-lg-2 p-lg-1 m-0">${names[x].genre}</small>
                                                 </div>
                                                 <div class="card-body d-grid">
-                                                <small class="card-title fw-bold fs-5">${games[x].title}</small>
-                                                <small class="card-text mb-4">${icon()} | ${games[x].release_date}</small>
-                                                <small class="card-text">${games[x].short_description}</small>
+                                                <small class="card-title fw-bold fs-5">${names[x].title}</small>
+                                                <small class="card-text mb-4">${icon()} | ${names[x].release_date}</small>
+                                                <small class="card-text">${names[x].short_description}</small>
                                                 </div>
                                                 <div class="card-footer d-flex justify-content-between align-items-center">
-                                                <a class="cardLink aLink" href="${games[x].freetogame_profile_url}">See more details</a>
+                                                <a class="cardLink aLink" href="${names[x].freetogame_profile_url}">See more details</a>
                                                 <div class="orangeText text-light rounded-1 border-0 p-lg-1 fw-bold ps-lg-2 pe-lg-2" >Free</div>
                                                 </div>
                                             </div>
@@ -111,7 +110,7 @@ const searchBtn = () => {
                     }
                     
             }
-        exercise2 = games;
+        exercise2 = names;
         
         }
         
